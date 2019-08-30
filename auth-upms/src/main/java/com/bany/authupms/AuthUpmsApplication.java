6362cf70a9eda7145b80fd9771725b1b1844c3b2
7F68B04D37A93A998410595AF9261973 <7F68B04D37A93A998410595AF9261973@i-search.com.cn>
2019-08-30 16:45:11 +0800

    Initial commit

---
+++ auth-upms/src/main/java/com/bany/authupms/AuthUpmsApplication.java
@@ -0,0 +1,13 @@
+package com.bany.authupms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUpmsApplication.class, args);
+    }
+
+}
